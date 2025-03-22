165f69063f2c93b826d3f48d0b115923180ef0a9
ngothetai <thetai2382002@gmail.com>
2025-03-22 22:14:48 +0700

    Fix DataLoader class

--- tweet_sentiment/load_data/data.py
+++ tweet_sentiment/load_data/data.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from typing import List
 from tweet_sentiment.model.data import Tweet
+from dataclasses import dataclass
+from loguru import logger
 
 
 class DataLoader:
@@ -11,20 +14,43 @@ class DataLoader:
 
     def __preprocess_data(self) -> None:
         try:
-            self.data = pd.read_csv(self.data_path)
+            self.data = pd.read_csv(
+                self.data_path,
+                encoding="latin-1",
+                names=["target", "ids", "date", "flag", "user", "text"],
+            )
         except FileNotFoundError:
-            raise FileNotFoundError(f"Data file not found at {data_path}")
+            raise FileNotFoundError(f"Data file not found at {self.data_path}")
         except pd.errors.EmptyDataError:
-            raise pd.errors.EmptyDataError(f"Data file is empty at {data_path}")
+            raise pd.errors.EmptyDataError(f"Data file is empty at {self.data_path}")
         except pd.errors.ParserError:
-            raise pd.errors.ParserError(f"Data file is not a valid CSV at {data_path}")
+            raise pd.errors.ParserError(
+                f"Data file is not a valid CSV at {self.data_path}"
+            )
         except Exception as e:
             raise Exception(f"An error occurred while loading data: {str(e)}")
 
     def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, i: int) -> Tweet:
-        return Tweet(
-            target=self.data.iloc[i]["target"], tweet=self.data.iloc[i]["text"]
-        )
+    def __getitem__(self, i) -> List[Tweet]:
+        res = []
+        logger.info(f"Getting item {i}")
+        for target, tweet in zip(
+            pd.Series(self.data.iloc[i]["target"]).values,
+            pd.Series(self.data.iloc[i]["text"]).values,
+        ):
+            res.append(Tweet(target=target, tweet=tweet))
+        return res
+
+    @classmethod
+    def split(
+        cls, data: "DataLoader", train_size: float = 0.8
+    ) -> Tuple["DataLoader", "DataLoader"]:
+        index = int(len(data) * train_size)
+        return (data[:index], data[index:])
+
+
+# Speed up crawl -> multiprocessing, multi-threading, asyncio
+# Storage -> x csv -> database (NoSQL, SQL)
+# 
